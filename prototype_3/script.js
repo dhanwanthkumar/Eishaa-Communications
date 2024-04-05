a70e2f7d9550a34bbbdfb69a25b98ea87836aafc
dhanwanthkumar <dhanwanthmohan@gmail.com>
2024-04-06 00:18:09 +0530

    prototype 3 is the published website

---
+++ prototype_3/script.js
@@ -0,0 +1,23 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        let video = slides[i].querySelector('video');
+        video.pause(); // Pause all videos when hiding the slide
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    let video = slides[slideIndex - 1].querySelector('video');
+    video.play(); // Play the video of the current slide
+    setTimeout(showSlides, 10100); // Change slide every 10 seconds
+}
